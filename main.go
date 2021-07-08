9a5733b49807a7f13c363d801cfde3f946ff4b8e
polarspetroll <polarspetroll@gmail.com>
2021-07-09 01:11:36 +0430

    cleanup

--- main.go
+++ main.go
@@ -9,17 +9,23 @@ import (
 )
 
 func main() {
-	token := os.Getenv("TOKEN")
+  token := os.Getenv("TOKEN")
 	webhookurl := "https://" + os.Getenv("DOMAIN") + "/" + token
+
 	server := &http.Server{
-		ReadTimeout:  25 * time.Second,
-		WriteTimeout: 25 * time.Second,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
 		Addr:         ":" + os.Getenv("PORT"),
 	}
+
 	http.Handle("/statics/", http.StripPrefix("/statics/", http.FileServer(http.Dir("statics"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
-	http.HandleFunc("/"+token, app.BotHandler)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.Redirect(w, r, "/index", 302) })
+
+	///////Webhook Handler/////////////
+	http.HandleFunc("/"+token, app.BotHandler)
+
+	///////// Main App Handlers////////////
 	http.HandleFunc("/index", app.Index)
 	http.HandleFunc("/login", app.Login)
 	http.HandleFunc("/logout", app.LogOut)
@@ -28,6 +34,8 @@ func main() {
 	http.HandleFunc("/list", app.List)
 	http.HandleFunc("/product", app.GetProduct)
 	http.HandleFunc("/new", app.NewProduct)
+	///////////////////////////////////////
+
 	telegram.SetWebhook(webhookurl)
 	server.ListenAndServe()
 }
